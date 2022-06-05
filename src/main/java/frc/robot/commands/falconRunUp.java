b04c37c34db585466331aab5e0cb415e0ffa73af
Richard Ho <richard.ho@metuchenschools.org>
2022-06-05 19:41:18 -0400

    changed subsystems and commands

--- src/main/java/frc/robot/commands/falconRunUp.java
+++ src/main/java/frc/robot/commands/falconRunUp.java
@@ -3,17 +3,17 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.*;
 import frc.robot.subsystems.*;
 
-public class falconRunDown extends CommandBase{
+public class falconRunUp extends CommandBase{
     private falcon falconMotor;
 
-    public falconRunDown(falcon subsystem) {
+    public falconRunUp(falcon subsystem) {
         addRequirements(subsystem);
         falconMotor = subsystem;
     }
 
     @Override
     public void execute() {
-        falconMotor.lower();
+        falconMotor.raise();
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/falcon.java
+++ src/main/java/frc/robot/subsystems/falcon.java
@@ -1,18 +1,30 @@
 package frc.robot.subsystems;
 
+import static frc.robot.Constants.*;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class falcon extends SubsystemBase{
-    private static final int deviceID = 1;
-    private TalonFX motor;
+    private final TalonFX falconMotor = new TalonFX(FALCON_ID);;
 
     public falcon() {
-        motor = new TalonFX(deviceID);
-        motor.configFactoryDefault();
+        falconMotor.configFactoryDefault();
     }
 
+    public void raise() {
+        falconMotor.set(ControlMode.PercentOutput, FALCON_SPEED); // Goes up
+    }
+
+    public void lower() {
+        falconMotor.set(ControlMode.PercentOutput, -FALCON_SPEED); // Goes down
+    }
+
+    public void stop() {
+        falconMotor.set(ControlMode.PercentOutput, 0); // Stops
+    }
     @Override
     public void periodic() {
 
